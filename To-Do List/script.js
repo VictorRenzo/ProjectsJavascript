e91b1b1e72bd858af95c068c5f2e7350e123f702
VictorReno <v.renzo@usp.br>
2022-01-18 14:53:20 -0300

    add modal todo list

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -36,12 +36,60 @@ function createItem(){
   itemsList.prepend(taskItemContainer);
 }
 
+function createItemModal(){
+  let modal = document.createElement("div");
+  modal.classList.add("modal");
+  document.body.prepend(modal);
+
+  document.querySelector(".addItem").disabled = true;
+
+  let forms = document.createElement("div");
+  forms.classList.add("formsModal");
+  modal.appendChild(forms);
+
+  let modalTitle = document.createElement("p");
+  modalTitle.classList.add("modalTitle");
+  modalTitle.innerText = "Add task";
+  forms.appendChild(modalTitle);
+
+  let inputContainer = document.createElement("div");
+  inputContainer.classList.add("inputContainer");
+
+  let inputTitle = document.createElement("input");
+  inputTitle.type = "text";
+  inputTitle.classList.add("inputItemTitle");
+  inputTitle.placeholder = "Task Title";
+
+  let inputDescription = document.createElement("input");
+  inputDescription.type = "text";
+  inputDescription.classList.add("inputItemDescription");
+  inputDescription.placeholder = "Task Description";
+
+  
+  let buttonsModalContainer = document.createElement("div");
+  buttonsModalContainer.classList.add("buttonsModalContainer");
+  
+  let buttonCancel = document.createElement("button");
+  buttonCancel.classList.add("buttonCancelAdd");
+  buttonCancel.innerText = "Cancel";
+  buttonCancel.addEventListener("click", function(){ closeModal();} );
+  let buttonConfirmAdd = document.createElement("button");
+  buttonConfirmAdd.classList.add("buttonConfirmAdd");
+  buttonConfirmAdd.innerText = "Add Task";
+  buttonConfirmAdd.addEventListener("click", function(){ confirmAddItem (inputContainer);});
+
+  forms.appendChild(inputContainer);
+  forms.appendChild(buttonsModalContainer);
+  inputContainer.appendChild(inputTitle);
+  inputContainer.appendChild(inputDescription);
+  buttonsModalContainer.appendChild(buttonCancel);
+  buttonsModalContainer.appendChild(buttonConfirmAdd);
+
+ }
+
 function confirmAddItem(taskParent){
   let title = taskParent.childNodes[0].value;
   let description = taskParent.childNodes[1].value;
-  console.log(title);
-  taskParent.childNodes[1].addEventListener("input", valorInput);
-
   let taskTitle = document.createElement("h3");
   let taskDescription = document.createElement("h4");
   taskTitle.classList.add("taskTitle");
@@ -49,6 +97,17 @@ function confirmAddItem(taskParent){
 
   taskTitle.innerText = title;
   taskDescription.innerText = description;
+
+  let itemsList = document.querySelector(".itemsList");
+  itemsList.prepend(taskTitle);
+  itemsList.prepend(taskDescription);
+
+  closeModal();
+} 
+
+function closeModal(){
+  document.querySelector(".modal").remove();
+  document.querySelector(".addItem").disabled = false;
 }
 
 function valorInput(e){
