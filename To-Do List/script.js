6c6737ae3abbebfba2920548e8d3cf5afc773b5f
VictorReno <v.renzo@usp.br>
2022-01-18 12:22:44 -0300

    changes todo list

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -1,21 +1,59 @@
-function createItem(){
-  let taskItem = document.createElement("div");
-  taskItem.classList.add("item");
+function doneItem(){
+}
 
-  let itemsList = document.querySelector(".itemsList");
-  itemsList.prepend(taskItem);
+function removeItem(){
+}
+
+function createItem(){
+  let taskItemContainer = document.createElement("div");
+  taskItemContainer.classList.add("taskItemContainer");
+  taskItemContainer.classList.add("undone");
+
+  let taskItemTitle = document.createElement("textarea");
+  taskItemTitle.placeholder = "Título da tarefa";
+  taskItemTitle.classList.add("itemTitle");
+  taskItemContainer.appendChild(taskItemTitle);
+  
+  let taskItemDescription = document.createElement("textarea");
+  taskItemDescription.placeholder = "Descrição da tarefa";
+  taskItemDescription.classList.add("itemDescription");
+  taskItemContainer.appendChild(taskItemDescription);
 
   let totalElement = document.querySelector("#totalItems");
   totalElement.innerText++;
 
+  let taskItemAdd = document.createElement("button");
+  taskItemAdd.innerText = "Confirmar";
+  taskItemAdd.classList.add("buttonConfirm");
+  taskItemContainer.appendChild(taskItemAdd);
+  
+  taskItemAdd.addEventListener("click", function(){confirmAddItem(taskItemContainer)});
+
   let leftItems = document.querySelector("#leftItems");
   leftItems.innerText++;
+
+  let itemsList = document.querySelector(".itemsList");
+  itemsList.prepend(taskItemContainer);
 }
 
-function doneItem(){
+function confirmAddItem(taskParent){
+  let title = taskParent.childNodes[0].value;
+  let description = taskParent.childNodes[1].value;
+  console.log(title);
+  taskParent.childNodes[1].addEventListener("input", valorInput);
 
+  let taskTitle = document.createElement("h3");
+  let taskDescription = document.createElement("h4");
+  taskTitle.classList.add("taskTitle");
+  taskDescription.classList.add("taskDescription");
+
+  taskTitle.innerText = title;
+  taskDescription.innerText = description;
 }
 
-function removeItem(){
-
+function valorInput(e){
+  let x = e.target.value;
+  console.log(x);
 }
+
+
